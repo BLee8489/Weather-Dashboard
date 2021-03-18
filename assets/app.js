902b079fdbb18e6eafc2e2f1ca36edeca22bc402
“bonnielee” <bonlee8489@gmail.com>
2021-03-17 20:35:33 -0700

    define variables

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,12 @@
+// Define variables
 let apiKey = "30da43373157d45ad4440e9c0dc3b9d0"
 let cityNameInput = document.querySelector("#cityInput")
 let submitCityInput = document.querySelector("#submitCity")
 let currentCity = document.querySelector("#city")
+let currentCityTemp = document.querySelector("#temp")
+let currentCityHumid = document.querySelector("#humidity")
+let currentCityWind = document.querySelector("#windSpeed")
+let currentCityUv = document.querySelector("#uvIndex")
+let dayOneDate = document.querySelector("#cityOne5")
+let dayOneTemp = document.querySelector("#tempOne5")
+let dayOneHumid = document.querySelector("#humidityOne5")
