e986dc98a70ceb42db730bd7468b2e3b87bd2424
“bonnielee” <bonlee8489@gmail.com>
2021-03-17 21:22:43 -0700

    fetch url

--- assets/app.js
+++ assets/app.js
@@ -39,3 +39,32 @@ submitCityInput.addEventListener("click", function(e){
 })
 
 // Fetch 5 day url and append values to HTML 
+function fetchFiveDay(cityValue) {
+  if(!cityValue){
+    cityValue = "oakland"
+  }
+  let fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityValue + "&appid=" + apiKey + "&units=imperial"
+    fetch(fiveDayUrl)
+      .then(function (response) {
+        return response.json();
+        
+      })
+      .then(function (data) {
+        console.log("five day", data);
+        dayOneDate.innerHTML = new Date(data.list[4].dt_txt).toLocaleString() 
+        dayOneTemp.innerHTML = "Temerature: " + data.list[4].main.temp + " °F"
+        dayOneHumid.innerHTML = "Humidity: " + data.list[4].main.humidity + "%"
+        dayTwoDate.innerHTML = new Date(data.list[12].dt_txt).toLocaleString() 
+        dayTwoTemp.innerHTML = "Temerature: " + data.list[12].main.temp + " °F"
+        dayTwoHumid.innerHTML = "Humidity: " + data.list[12].main.humidity + "%"
+        dayThreeDate.innerHTML = new Date(data.list[20].dt_txt).toLocaleString() 
+        dayThreeTemp.innerHTML = "Temerature: " + data.list[20].main.temp + " °F"
+        dayThreeHumid.innerHTML = "Humidity: " + data.list[20].main.humidity + "%"
+        dayFourDate.innerHTML = new Date(data.list[28].dt_txt).toLocaleString() 
+        dayFourTemp.innerHTML = "Temerature: " + data.list[28].main.temp + " °F"
+        dayFourHumid.innerHTML = "Humidity: " + data.list[28].main.humidity + "%"
+        dayFiveDate.innerHTML = new Date(data.list[36].dt_txt).toLocaleString() 
+        dayFiveTemp.innerHTML = "Temerature: " + data.list[36].main.temp + " °F"
+        dayFiveHumid.innerHTML = "Humidity: " + data.list[36].main.humidity + "%"
+      });
+};
