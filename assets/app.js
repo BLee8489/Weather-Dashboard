fadd7f1f2dceca1691470b8248cfc9245e92036f
“bonnielee” <bonlee8489@gmail.com>
2021-03-17 20:29:08 -0700

    starting this app back up

---
+++ assets/app.js
@@ -0,0 +1,4 @@
+let apiKey = "30da43373157d45ad4440e9c0dc3b9d0"
+let cityNameInput = document.querySelector("#cityInput")
+let submitCityInput = document.querySelector("#submitCity")
+let currentCity = document.querySelector("#city")
